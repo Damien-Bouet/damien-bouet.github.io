2ab6047762ec0cefb7b21219acafb88df6bdda92
Damien Bouet <bouet.dam@gmail.com>
2025-05-29 14:54:13 +0200

    menu vertical

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function openPage(url) {
 
 function scrollToElement(element_id){
   document.getElementById(element_id).scrollIntoView({ behavior: "smooth" });
+  toggleMenu();
 }
 
 window.onload = function () {
@@ -13,3 +14,16 @@ window.onload = function () {
     document.getElementById(scrollPos).scrollIntoView({ behavior: "smooth" });
   }
 };
+
+function toggleMenu() {
+  document.getElementById('menu').classList.toggle('show');
+}
+
+// Close the menu when clicking outside
+document.addEventListener('click', function(event) {
+  const menu = document.getElementById('menu');
+  const menuButton = document.getElementById('menu-button');
+  if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
+    menu.classList.remove('show');
+  }
+});
